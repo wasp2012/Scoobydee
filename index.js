a1cdc27e4cda1c3cab0587e1b708c9c87613fbd0
Youssef wael <y.sayed@cch-kuwait.com>
2024-01-03 21:33:50 +0200

    3/1/24
    -Finished Home Page

--- index.js
+++
@@ -1,7 +0,0 @@
-function getPageName(url) {
-    var index = url.lastIndexOf("/") + 1;
-    var filenameWithExtension = url.substr(index);
-    var filename = filenameWithExtension.split(".")[0]; // <-- added this line
-    console.log(filename);
-    return filename;                                    // <-- added this line
-}
